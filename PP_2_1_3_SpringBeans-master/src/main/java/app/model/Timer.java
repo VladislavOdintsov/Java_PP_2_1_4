4dc77d1965a83b9c86b990c6008ac2afc9d0df59
VladislavOdintsov <youngmonkey@yandex.ru>
2022-11-23 21:24:45 +0300

    Commit_1

--- PP_2_1_3_SpringBeans-master/src/main/java/app/model/Timer.java
+++ PP_2_1_3_SpringBeans-master/src/main/java/app/model/Timer.java
@@ -1,6 +1,19 @@
 package app.model;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Timer {
+
+//    @Autowired
+//    @Qualifier("animalsCage")
+//    private Timer timer;
+//
+//    public Timer getTimer() {
+//        return timer;
+//    }
 
     private Long nanoTime = System.nanoTime();
 
